85b80240efe733473e5ac75c0bd2737a9b9b88ea
Allahsukur <10745@holbertonstudents.com>
2025-04-06 14:29:56 +0000

    56789

--- task.c
+++ task.c
@@ -1,48 +1,68 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void print_int(int n) {
-    if (n < 0) {
-        write(1, "-", 1);  /* Print minus sign for negative numbers */
-        n = -n;  /* Make n positive */
+void print_int(int n)
+{
+    char buffer[10];
+    int i;
+    int j;
+    int num;
+
+    i = 0;
+
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        num = -n;
+    }
+    else
+    {
+        num = n;
     }
 
-    char buffer[10];
-    int i = 0;
-
-    /* Convert integer to string (reversed) */
-    if (n == 0) {
+    if (num == 0)
+    {
         write(1, "0", 1);
         return;
     }
 
-    while (n > 0) {
-        buffer[i++] = (n % 10) + '0';  /* Get the last digit and convert to ASCII */
-        n = n / 10;
+    while (num > 0)
+    {
+        buffer[i] = (num % 10) + '0';
+        num = num / 10;
+        i++;
     }
 
-    /* Reverse the string and print it */
-    int j;
-    for (j = i - 1; j >= 0; j--) {
+    for (j = i - 1; j >= 0; j--)
+    {
         write(1, &buffer[j], 1);
     }
 }
 
-/* Example of how your printf function might look: */
-int my_printf(const char *format, ...) {
+int my_printf(const char *format, ...)
+{
     va_list args;
+    int i;
+    int num;
+
     va_start(args, format);
-    int i = 0;
-    while (format[i]) {
-        if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i')) {
-            int num = va_arg(args, int);
+    i = 0;
+
+    while (format[i])
+    {
+        if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+        {
+            num = va_arg(args, int);
             print_int(num);
-            i++;  /* Skip the %d or %i */
-        } else {
+            i++; /* skip the format character */
+        }
+        else
+        {
             write(1, &format[i], 1);
         }
         i++;
     }
+
     va_end(args);
-    return 0;
+    return (0);
 }
