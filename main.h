4279dda269e4ce1a442b95b2726370acaa5ffa93
Allahsukur <10745@holbertonstudents.com>
2025-04-06 10:57:45 +0000

    5678

--- main.h
+++ main.h
@@ -1,36 +1,6 @@
-#include <stdarg.h>
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int count = 0;
-    const char *ptr;
+int _printf(const char *format, ...);
 
-    va_start(args, format);
-
-    for (ptr = format; *ptr != '\0'; ptr++) {
-        if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' || *(ptr + 1) == '%')) {
-            ptr++;  // Move past '%' to check the specifier
-            if (*ptr == 'c') {
-                char c = va_arg(args, int);  // 'c' expects an int, not a char
-                write(1, &c, 1);
-                count++;
-            } else if (*ptr == 's') {
-                char *str = va_arg(args, char*);
-                for (int i = 0; str[i] != '\0'; i++) {
-                    write(1, &str[i], 1);
-                    count++;
-                }
-            } else if (*ptr == '%') {
-                write(1, "%", 1);
-                count++;
-            }
-        } else {
-            write(1, ptr, 1);  // Regular character
-            count++;
-        }
-    }
-
-    va_end(args);
-    return count;
-}
+#endif
